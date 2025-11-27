638113aeb0a2d202f46fa9aa8e2483ebf6b5467b
yoni10179 <yonatan.medvedev@gmail.com>
2025-11-27 18:41:17 +0200

    Add Skeleton Project

---
+++ TechSystem26a1.cpp
@@ -0,0 +1,55 @@
+// You can edit anything you want in this file.
+// However you need to implement all public StudentCourseManager function, as provided below as a template
+
+#include "TechSystem26a1.h"
+
+
+TechSystem::TechSystem()
+{
+    
+}
+
+TechSystem::~TechSystem()
+{
+    
+}
+
+StatusType TechSystem::addStudent(int studentId)
+{
+    return StatusType::FAILURE;
+}
+
+StatusType TechSystem::removeStudent(int studentId)
+{
+    return StatusType::FAILURE;
+}
+
+StatusType TechSystem::addCourse(int courseId, int points)
+{
+    return StatusType::FAILURE;
+}
+
+StatusType TechSystem::removeCourse(int courseId)
+{
+    return StatusType::FAILURE;
+}
+
+StatusType TechSystem::enrollStudent(int studentId, int courseId)
+{
+    return StatusType::FAILURE;
+}
+
+StatusType TechSystem::completeCourse(int studentId, int courseId)
+{
+    return StatusType::FAILURE;
+}
+
+StatusType TechSystem::awardAcademicPoints(int points)
+{
+    return StatusType::FAILURE;
+}
+
+output_t<int> TechSystem::getStudentPoints(int studentId)
+{
+    return 0;
+}
